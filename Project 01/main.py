2f358442ca235c3aa18947cb754fe3c15491e1ca
Sam Haberkorn <haberkornsam@gmail.com>
2020-09-12 12:56:59 -0600

    Update main.py

--- Project 01/main.py
+++ Project 01/main.py
@@ -37,11 +37,11 @@ def main():
 
 
 def transpose_matrix(matrix):
-    '''
+    """
     Transposes a matrix from (x, y) to (y, x)
     :param matrix: Matrix
     :return: Matrix
-    '''
+    """
     new_matrix = []
     for i in range(len(matrix[0])):
         new_matrix.append(([None] * len(matrix)).copy())
@@ -54,12 +54,12 @@ def transpose_matrix(matrix):
 
 
 def subtract_matrices(matrix_a, matrix_b):
-    '''
+    """
     Subtracts first matrix from second matrix
     :param matrix_a: Matrix (2D-List)
     :param matrix_b: Matrix (2D-List)
     :return: Matrix (2D-List)
-    '''
+    """
     new_matrix = []
     for rowA, rowB in zip(matrix_a, matrix_b):
         temp = []
@@ -70,12 +70,12 @@ def subtract_matrices(matrix_a, matrix_b):
 
 
 def multiply_matrix(matrix, multiplier):
-    '''
+    """
     Multiplies a matrix by a constant value
     :param matrix: Matrix to multiply
     :param multiplier: multiplier
     :return: a new matrix (2D-List)
-    '''
+    """
     new_matrix = []
     for row in matrix:
         temp = []
@@ -86,34 +86,33 @@ def multiply_matrix(matrix, multiplier):
 
 
 def print_matrix(matrix):
-    '''
+    """
     Prints matrix to stdout with 5 characters per cell
     :param matrix: matrix to write
     :return: None
-    '''
+    """
     for row in matrix:
         print(" ".join(map(lambda n:  f"{n:>5}", row)))
 
 
 def write_matrix(matrix, character, file):
-    '''
+    """
     Writes matrix to file
     :param matrix: Matrix to write
     :param character: Character name of matrix
     :param file: file to write to (must be open)
     :return: None
-    '''
+    """
     file.write(f"{character} {len(matrix)} {len(matrix[0])} ")
     for row in matrix:
         file.write(" ".join(map(str, row)))
         file.write(" ")
 
 
-
 def read_matrix(file):
-    '''
+    """
     Reads a matrix from a file. Returns a tuple with the character name and the actual matrix
-    '''
+    """
     matrix = []
 
     char = get_next_char(file)
@@ -127,18 +126,21 @@ def read_matrix(file):
         matrix.append(temp)
     return char, matrix
 
+
 '''
 Recursive function to read in each set of characters.
-If no character or whitespace is read, return the read charactors or keep reading (if no characters are read)
+If no character or whitespace is read, return the read characters or keep reading (if no characters are read)
 '''
+
+
 def get_next_char(file, prev_chars=""):
-    '''
+    """
     Recursive function to read in each set of characters.
-    If no character or whitespace is read, return the read charactors or keep reading (if no characters are read)
+    If no character or whitespace is read, return the read characters or keep reading (if no characters are read)
     :param file: file
     :param prev_chars: str
     :return: str
-    '''
+    """
     char = file.read(1)
 
     if not len(char) == 0 and char not in [None, " ", "\n"]:
